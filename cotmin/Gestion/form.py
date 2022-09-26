be207248d75df123782f2b2c041030d448771458
Rodriguez Juan <stylorj@gmail.com>
2022-09-26 13:58:08 -0300

    update style views

--- cotmin/Gestion/form.py
+++ cotmin/Gestion/form.py
@@ -1,11 +1,19 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Tramites
 
 
-class TramiteForm(ModelForm):
+class TramiteForm(forms.ModelForm):
     class Meta:
         model = Tramites
         fields = ['number', 'tipo', 'solicitante', 'nota_by_admin']
+        widgets = {
+            'number': forms.NumberInput(attrs={'class': 'form-control',
+                                               'placeholder': 'Escribe numero del expediente'}),
+            'solicitante': forms.TextInput(attrs={'class': 'form-control',
+                                               'placeholder': 'Nombre del responsable'}),
+            'nota_by_admin': forms.Textarea(attrs={'class': 'form-control',
+                                               'placeholder': 'Espacio para notas'}),
+        }
 
 
 
